b91a493b33c7870e5d50c8b5e5a228074667c6c6
Abdul Wahab <142666026+smabdulwahab@users.noreply.github.com>
2024-11-11 13:33:02 +0500

    add app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+const BASE_URL = `https://v6.exchangerate-api.com/v6/a09a45a03b0d6baea1830194/latest/USD`;
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "PKR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  try {
+    let response = await fetch(BASE_URL);
+    let data = await response.json();
+
+    if (data && data.conversion_rates) {
+      let rate = data.conversion_rates[toCurr.value];
+      let finalAmount = (amtVal * rate).toFixed(2);
+      msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    } else {
+      msg.innerText = "Error fetching conversion rates. Please try again.";
+    }
+  } catch (error) {
+    msg.innerText =
+      "Unable to fetch data. Please check your connection or API key.";
+  }
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
